2871aac70506ffa73b3e7bac321edc423ce55891
chanbopich11tann <143944220+chanbopich11tann@users.noreply.github.com>
2023-10-08 23:00:38 -0400

    Update my-function.js

--- my-function.js
+++ my-function.js
@@ -5,23 +5,16 @@ const app = express();
 const port = 3000;
 
 app.get('/say', async (req, res) => {
-    const { keyword } = req.query;
+  const keyword = req.query.keyword;
 
-    if (!keyword) {
-        return res.status(400).json({ error: 'Keyword parameter is missing.' });
-    }
+  if (!keyword) {
+    return res.status(400).json({ error: 'Keyword parameter is missing.' });
+  }
 
-    try {
-        // You can use Axios to make an HTTP request to another service or API here if needed.
-        // For simplicity, we'll just concatenate the keyword to "TANN says".
-        const result = `TANN says ${keyword}`;
-        res.json({ message: result });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred.' });
-    }
+  const result = `TANN says ${keyword}`;
+  res.json({ message: result });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
